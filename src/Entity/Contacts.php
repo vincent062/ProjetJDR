05efcbb51c9324525ea5da3ecd9fb097cfe8c875
vincent <vince062580@gmail.com>
2025-06-13 10:08:20 +0200

    a

---
+++ src/Entity/Contacts.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ContactsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ContactsRepository::class)]
+class Contacts
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $prénom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $message = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrénom(): ?string
+    {
+        return $this->prénom;
+    }
+
+    public function setPrénom(string $prénom): static
+    {
+        $this->prénom = $prénom;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): static
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Table.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TableRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TableRepository::class)]
+#[ORM\Table(name: '`table`')]
+class Table
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $MJ = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Licence = null;
+
+    #[ORM\Column]
+    private ?int $places = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $jours_joués = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $horaires = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $type_de_table = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMJ(): ?string
+    {
+        return $this->MJ;
+    }
+
+    public function setMJ(string $MJ): static
+    {
+        $this->MJ = $MJ;
+
+        return $this;
+    }
+
+    public function getLicence(): ?string
+    {
+        return $this->Licence;
+    }
+
+    public function setLicence(string $Licence): static
+    {
+        $this->Licence = $Licence;
+
+        return $this;
+    }
+
+    public function getPlaces(): ?int
+    {
+        return $this->places;
+    }
+
+    public function setPlaces(int $places): static
+    {
+        $this->places = $places;
+
+        return $this;
+    }
+
+    public function getJoursJoués(): ?string
+    {
+        return $this->jours_joués;
+    }
+
+    public function setJoursJoués(?string $jours_joués): static
+    {
+        $this->jours_joués = $jours_joués;
+
+        return $this;
+    }
+
+    public function getHoraires(): ?string
+    {
+        return $this->horaires;
+    }
+
+    public function setHoraires(string $horaires): static
+    {
+        $this->horaires = $horaires;
+
+        return $this;
+    }
+
+    public function getTypeDeTable(): ?string
+    {
+        return $this->type_de_table;
+    }
+
+    public function setTypeDeTable(string $type_de_table): static
+    {
+        $this->type_de_table = $type_de_table;
+
+        return $this;
+    }
+}
