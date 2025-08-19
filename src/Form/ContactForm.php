fba5ca04e1f2c2e0a18a7242bcb8624fdff5fec2
vincent <vince062580@gmail.com>
2025-08-19 15:10:48 +0200

    19/08

--- src/Form/ContactForm.php
+++ src/Form/ContactForm.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Contacts;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
